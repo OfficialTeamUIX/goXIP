f169e8e65e131563678b5c43e6570c7e75593c0c
Milenko <39630670+MrMilenko@users.noreply.github.com>
2023-04-04 15:32:23 -0400

    Update goxip.go
    Added a creation function.

--- goxip.go
+++ goxip.go
@@ -1,4 +1,4 @@
-// Put together using bubblegum and ductape by Milenko @ TeamUIX
+// Put together using bubblegum and ductape by @dtoxmilenko
 // This is a Go port of unxip by MaTiAz5
 // Using references from Pixit by Voltaic
 package main
@@ -110,7 +110,7 @@ func extractArchive(archiveFile, extractFolder string) error {
 	for i, fd := range fileData {
 		filename := getFilename(filenameBlock, int(fileNames[i].NameOffset))
 		dstPath := filepath.Join(extractFolder, strings.ReplaceAll(filename, "\\", string(filepath.Separator)))
-		if fd.Type == 5 || fd.Type == 6 || fd.Type == 4 { // Skipping buffer files, and Mesh files. Extraction of mesh's is broken.
+		if fd.Type == 5 || fd.Type == 6 || fd.Type == 4 { // Skipping buffer files, and Mesh files. Extraction of mesh's is broken. But it appears to be working on creating XIP files.
 			fmt.Println("Skipping file of", filename, "which is a", fileTypes[fd.Type])
 			continue
 		}
@@ -151,24 +151,147 @@ func extractArchive(archiveFile, extractFolder string) error {
 
 	return nil
 }
+func createXIP(folder, archiveFile string) error {
+	var fileData []FileData
+	var fileNames []FileName
+	var filenameBlock bytes.Buffer
+	var fileContentBlock bytes.Buffer
+
+	err := filepath.Walk(folder, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if info.IsDir() {
+			return nil
+		}
+
+		relPath, err := filepath.Rel(folder, path)
+		if err != nil {
+			return err
+		}
+
+		data, err := ioutil.ReadFile(path)
+		if err != nil {
+			return err
+		}
+
+		fileData = append(fileData, FileData{
+			Offset: uint32(fileContentBlock.Len()),
+			Size:   uint32(len(data)),
+			Type:   0, // Set the file type as needed. Here, we use 0 (Generic) for all files. See the fileTypes map in extractArchive() for more information.
+		})
+
+		fileNames = append(fileNames, FileName{
+			DataIndex:  uint16(len(fileData) - 1),
+			NameOffset: uint16(filenameBlock.Len()),
+		})
+
+		filenameBlock.WriteString(strings.ReplaceAll(relPath, string(filepath.Separator), "\\"))
+		filenameBlock.WriteByte(0)
+		fileContentBlock.Write(data)
+
+		// Output the file name being added to the XIP archive
+		fmt.Printf("Adding file '%s' ...\n", relPath)
+
+		return nil
+	})
+
+	if err != nil {
+		return err
+	}
+
+	header := XIPHeader{
+		Magic:      [4]byte{'X', 'I', 'P', '0'},
+		DataOffset: uint32(binary.Size(XIPHeader{}) + len(fileData)*binary.Size(FileData{}) + len(fileNames)*binary.Size(FileName{}) + filenameBlock.Len()),
+		Files:      uint16(len(fileData)),
+		Names:      uint16(len(fileNames)),
+		DataSize:   uint32(fileContentBlock.Len()),
+	}
+
+	outputFile, err := os.Create(archiveFile)
+	if err != nil {
+		return err
+	}
+	defer outputFile.Close()
+
+	err = binary.Write(outputFile, binary.LittleEndian, header)
+	if err != nil {
+		return err
+	}
+
+	for _, fd := range fileData {
+		err = binary.Write(outputFile, binary.LittleEndian, fd)
+		if err != nil {
+			return err
+		}
+	}
+
+	for _, fn := range fileNames {
+		err = binary.Write(outputFile, binary.LittleEndian, fn)
+		if err != nil {
+			return err
+		}
+	}
+
+	_, err = outputFile.Write(filenameBlock.Bytes())
+	if err != nil {
+		return err
+	}
+
+	_, err = outputFile.Write(fileContentBlock.Bytes())
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
 
 func main() {
-
-	if len(os.Args) < 2 {
-		fmt.Printf("Usage: %s <archive>\n", os.Args[0])
+	if len(os.Args) < 3 {
+		fmt.Printf("Usage: goxip <command> <input>\n")
+		fmt.Println("Commands:")
+		fmt.Println("  extract: Extracts all files from an XIP archive into a folder.")
+		fmt.Println("  create: Creates a new XIP archive from a folder.")
+		fmt.Println("Examples:")
+		fmt.Println("  Extract a XIP archive: goxip extract sample.xip")
+		fmt.Println("  Create a XIP archive: goxip create sample_folder")
 		os.Exit(1)
 	}
 
-	// Take a filename as input and extract it to an output folder of the same name.
+	command := os.Args[1]
+	input := os.Args[2]
 
-	archiveFile := os.Args[1]
-	// output folder is the same name as the archive file, without the extension, inside output directory.
-	extractFolder := "output/" + strings.TrimSuffix(archiveFile, filepath.Ext(archiveFile))
-	err := extractArchive(archiveFile, extractFolder)
-	if err != nil {
-		fmt.Printf("Error: %v\n", err)
+	switch command {
+	case "extract":
+		extractFolder := "output/" + strings.TrimSuffix(input, filepath.Ext(input))
+		err := extractArchive(input, extractFolder)
+		if err != nil {
+			fmt.Printf("Error: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Println("Extraction complete.")
+
+	case "create":
+		// Create the output/newfolder directory if it doesn't exist
+		err := os.MkdirAll("output/newfolder", 0755)
+		if err != nil {
+			fmt.Printf("Error creating directory: %v\n", err)
+			os.Exit(1)
+		}
+
+		// Save the new archive inside the "output/newfolder" directory
+		archiveFile := filepath.Join("output", "newfolder", filepath.Base(input)+".xip")
+		err = createXIP(input, archiveFile)
+		if err != nil {
+			fmt.Printf("Error: %v\n", err)
+			os.Exit(1)
+		}
+		fmt.Println("XIP archive created.")
+
+	default:
+		fmt.Printf("Unknown command: %s\n", command)
+		fmt.Printf("Usage: goxip <command> <input>\nCommands: extract, create\n")
 		os.Exit(1)
 	}
-
-	fmt.Println("Extraction complete.")
 }
